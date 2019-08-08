4c2b4efe923886bb4e88ad469a502abdb0d22b16
CianLR <cian.ruane1@gmail.com>
2019-08-08 21:48:20 +0100

    Add proper random dep and general cleanups

--- src/algos/dfs.rs
+++ src/algos/dfs.rs
@@ -1,39 +1,20 @@
-use std::time::SystemTime;
+use rand::prelude::*;
 
 use crate::algos::algo;
 use crate::maze::{Maze, Walls, wall_opposite};
 
 pub struct DfsAlgo;
 
-// TODO: Get a way better random func
-fn rand_num(mx: u64) -> u64 {
-    let t = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_millis();
-    let a = (t % 100007) % mx as u128;
-    a as u64
-}
-
-fn get_adjacent(sz: usize, x: usize, y: usize) -> Vec<(usize, usize, Walls)> {
-    let mut v = vec![];
-    if x > 0 { v.push((x - 1, y, Walls::Left)); }
-    if y > 0 { v.push((x, y - 1, Walls::Up)); }
-    if x + 1 < sz { v.push((x + 1, y, Walls::Right)); }
-    if y + 1 < sz { v.push((x, y + 1, Walls::Down)); }
-    v
-}
-
 impl DfsAlgo {
     fn dfs(maze: &mut Maze, visited: &mut Vec<Vec<bool>>,
            sz: usize, x: usize, y: usize) -> Result<(), String> {
-        let mut adj: Vec<(usize, usize, Walls)> = get_adjacent(sz, x, y)
-            .iter()
-            .filter(| (nx, ny, _) | !visited[*nx][*ny])
-            .map(| p | *p)
+        let mut adj: Vec<(usize, usize, Walls)> = DfsAlgo::get_adjacent(sz, x, y)
+            .into_iter()
+            .filter(|&(nx, ny, _)| !visited[nx][ny])
             .collect::<Vec<(usize, usize, Walls)>>();
+        adj.shuffle(&mut rand::thread_rng());
         while !adj.is_empty() {
-            let (nx, ny, d) = adj.remove(rand_num(adj.len() as u64) as usize);
+            let (nx, ny, d) = adj.pop().unwrap();
             if visited[nx][ny] {
                 continue;
             }
@@ -43,6 +24,15 @@ impl DfsAlgo {
             DfsAlgo::dfs(maze, visited, sz, nx, ny)?;
         }
         Ok(())
+    }
+
+    fn get_adjacent(sz: usize, x: usize, y: usize) -> Vec<(usize, usize, Walls)> {
+        let mut v = vec![];
+        if x > 0 { v.push((x - 1, y, Walls::Left)); }
+        if y > 0 { v.push((x, y - 1, Walls::Up)); }
+        if x + 1 < sz { v.push((x + 1, y, Walls::Right)); }
+        if y + 1 < sz { v.push((x, y + 1, Walls::Down)); }
+        v
     }
 }
 
